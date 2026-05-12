d410d628c8158af2b3197bd5d1a6c636d37ecf74
kwzcwl <kwolczanskizub@student.agh.edu.pl>
2026-05-12 14:19:18 +0200

    nastepny

--- main.cpp
+++ main.cpp
@@ -1,40 +1,71 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <chrono>
+#include <ctime>
 
 using namespace std;
 
-
-
-int menulogin() // dodaæ excption i aerror handling
+struct User
 {
-	cout<<"1 - Login"<<endl<<"2 - Create account"<<endl;
-	int input = '';
-	cin>>input;
-	
-	switch (input)
-	{
-		case 1:
-			
-			break;
-		case 2:
+    private:
+        vector<Workout> workouts;
+        string password;
+        
+    public:
+        string name;
+        string username;
+    
+};
 
-			break;
-		default:
-			cout<<"Invalid input: "<<input<<endl;
-			return menulogin();
-	}
-	
+struct Workout
+{
+    private:
+        vector<Excercise> excercises;
+        string date;
+};
+
+struct Excercise
+{
+    private:
+        string name;
+        map<int,Set> sets;
+};
+
+//struct WeightExcercise{};
+
+//struct CardioExcercise{};
+
+struct Set
+{
+    private:
+        int reps;
+        float weight;
+        int RPE;
+        string comment;
+    
+    
+};
+
+//struct Analyzer{};
+
+
+time_t getTime() {
+    auto now = system_clock::now();
+    return system_clock::to_time_t(now);
+}
+
+string formatDate(time_t date) {
+    char buffer[80];
+    // Bezpieczna konwersja czasu na lokalny
+    tm* timeinfo = std::localtime(&date);
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d", timeinfo);
+    return string(buffer);
 }
 
 int main()
 {
-	cout<<"=============================="<<endl<<"/tGYM GAINS APP/t"<<endl<<"=============================="<<endl;
-	
-	int i = 0;
 
-	while(i != 0)
-	{
-		i = menulogin();
-	}
-	
 	return 0;	
 }
